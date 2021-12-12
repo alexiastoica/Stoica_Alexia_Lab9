26e9dfa330f92f862e19e9fb0ccce936ab975d11
Stoica Alexia <alexiastoica@yahoo.com>
2021-12-12 21:03:39 +0200

    Add project files.

---
+++ Data/Stoica_Alexia_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stoica_Alexia_Lab8.Models;
+
+namespace Stoica_Alexia_Lab8.Data
+{
+    public class Stoica_Alexia_Lab8Context : DbContext
+    {
+        public Stoica_Alexia_Lab8Context (DbContextOptions<Stoica_Alexia_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stoica_Alexia_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Stoica_Alexia_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Stoica_Alexia_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Stoica_Alexia_Lab8.Data;
+using Stoica_Alexia_Lab8.Models;
+
+namespace Stoica_Alexia_Lab8.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Stoica_Alexia_Lab8.Data.Stoica_Alexia_Lab8Context _context;
+
+        public IndexModel(Stoica_Alexia_Lab8.Data.Stoica_Alexia_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
